2eb6fcb934a719c72f727af82193faff816e9977
sauromay <justineislearning@gmail.com>
2022-06-12 01:00:40 +0300

    Simple Basics

---
+++ app.py
@@ -0,0 +1,16 @@
+#Python is fun to learn.
+#All comments are done starting with a # 
+
+#STRINGS
+
+name = 'Justine'
+
+#to print the string we use the print function
+
+print(name)
+
+#FUNCTIONS
+
+def calcArea(radius):
+    #area of a circle is Pi * r * r
+    area = 3.142 * radius **2
